aad21a2d781bfb6ac6f0777262f75905e83417b2
root <vanielpdc@gmail.com>
2021-09-24 21:47:07 -0300

    adds the base of the project, and the function of registering stocks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Appbar from './Appbar'
+import Appbody from './Appbody'
+
+const App = () => {
+  return (
+    <div className="container-fluid">
+      <Appbar />
+      <Appbody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Appbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Appbar = () =>{
+    return(
+        <div className="rouw">
+            <div className= "col-sm-12 bg-danger py-2">
+                <h1 className="text-white">
+                    My Stocks
+                </h1>
+                <h4 className="text-white">
+                    Sistema de Cadastro de Portfolio de Ações 
+                </h4>
+            </div>
+        </div>
+    )
+}
+
+export default Appbar
