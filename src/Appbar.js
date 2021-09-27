5f1ee2074b301402ac023192e5533caf88a6b717
root <vanielpdc@gmail.com>
2021-09-27 18:33:00 -0300

    Adds search function and notifications

--- src/Appbar.js
+++ src/Appbar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./table.css"
 
 const Appbar = () =>{
     return(
         <div className="rouw">
-            <div className= "col-sm-12 bg-danger py-2">
+            <div className= "col-sm-12 py-2 bg-danger ">
                 <h1 className="text-white">
                     My Stocks
                 </h1>
